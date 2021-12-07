a8b16e4537e1c30ea3d748d193800ef981687e00
Sreejith <sreejithag97@gmail.com>
2021-12-07 23:25:07 +0530

    fixed

---
+++ migrations/20211207171123-create-user.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      salt: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
--- models/user.js
+++ models/user.js
@@ -1,38 +1,39 @@
-const { DataTypes, Model } = require("sequelize");
-const bcrypt = require("bcrypt");
-const { sequelize } = require("../configs/dbConfig");
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    validatePassword = async (password) => {
+      const res = await bcrypt.compare(password, this.password);
+      return res;
+    };
 
-class User extends Model {
-  validatePassword = async (password) => {
-    const res = await bcrypt.compare(password, this.password);
-    return res;
-  };
-}
-User.init(
-  {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    salt: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: "users1",
-    hooks: {
-      beforeCreate: async (user) => {
-        user.salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, user.salt);
-      },
-    },
+    static associate(models) {
+      // define association here
+    }
   }
-);
-
-module.exports.User = User;
+  User.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: { type: DataTypes.STRING, primaryKey: true },
+      password: DataTypes.STRING,
+      salt: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+      hooks: {
+        beforeCreate: async (user) => {
+          user.salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, user.salt);
+        },
+      },
+    }
+  );
+  return User;
+};
