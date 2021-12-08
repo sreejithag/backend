3995ef1aff002dde761521d9aa143cf28e5a1c55
Sreejith <sreejithag97@gmail.com>
2021-12-08 16:51:25 +0530

    redis added

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,6 @@
-const { User } = require("../models/user");
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/dbConfig");
+const User = require("../models/user")(sequelize, DataTypes);
 
 const getUserFromEmail = async (email) => {
   try {
@@ -46,6 +48,23 @@ exports.checkUserAndValidatePassword = async (email, password) => {
 
   return {
     success: await user.validatePassword(password),
-    usernameDB: user.username,
+    emailDB: user.email,
   };
+};
+
+exports.updateUser = async (email, firstName, lastName) => {
+  const user = await getUserFromEmail(email);
+
+  if (user) {
+    const newUser = user.update({
+      firstName: firstName,
+      lastName: lastName,
+    });
+    if (newUser) {
+      return true;
+    }
+    return false;
+  }
+
+  return false;
 };
---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+const redis = require("redis");
+const client = redis.createClient({
+  password: "password",
+});
+client.connect();
+exports.setUser = async (email, firstName, lastName) => {
+  try {
+    const res = await client.set(
+      email,
+      JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+      })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.isUserAvailable = async (email) => {
+  try {
+    const response = await client.get(email);
+    if (response !== null) {
+      return true;
+    }
+    return false;
+  } catch (err) {
+    console.log(err);
+  }
+};
